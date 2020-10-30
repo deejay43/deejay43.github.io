a5f2d792d4f1ee475b33afe52815f2537f2d3fca
deejay43 <73723439+deejay43@users.noreply.github.com>
2020-10-30 19:59:15 -0300

    Add files via upload

---
+++ cadsistema.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Nome: <?php echo $_POST["Name"] ?>
+        Email: <?php echo $_POST["Email"] ?>
+        Telefone: <?php echo $_POST["Telefone"] ?>
+        Message: <?php echo $_POST["Mensagem"] ?>
+    </body>
+</html>
